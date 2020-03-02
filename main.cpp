daa4d03110a607af4101af9a4bdeac3f384e7306
Nevation <j209446@naver.com>
2020-03-02 00:19:19 -0800

    Fixed Bug

--- main.cpp
+++ main.cpp
@@ -52,8 +52,11 @@ int main(int argc, char* argv[]){
 		return -1;
 	}
 
-	map<pair<vector<u_char>, vector<u_char>>, EndpointData> packet_map;
-	set<pair<vector<u_char>, vector<u_char>>> key;
+	map<pair<vector<u_char>, vector<u_char>>, EndpointData> endpoints;
+	set<pair<vector<u_char>, vector<u_char>>> endpoint_key;
+
+    map<pair<vector<u_char>, vector<u_char>>, EndpointData> conversations;
+    set<pair<vector<u_char>, vector<u_char>>> conversation_key;
 
 	while (true){        
 		struct pcap_pkthdr* header;
@@ -64,6 +67,7 @@ int main(int argc, char* argv[]){
     
         pair<vector<u_char>, vector<u_char>> dst_endpoint;
         pair<vector<u_char>, vector<u_char>> src_endpoint;
+        pair<vector<u_char>, vector<u_char>> addr_to_addr;
 
         vector<u_char> dst_mac(packet, packet + 6);
         vector<u_char> dst_ip_addr(&packet[30], &packet[30] + 4);
@@ -74,37 +78,72 @@ int main(int argc, char* argv[]){
 
         dst_endpoint = make_pair(dst_mac, dst_ip_addr);
         src_endpoint = make_pair(src_mac, src_ip_addr);
+        addr_to_addr = make_pair(dst_mac, src_mac);
 
-        key.insert(dst_endpoint);
-        key.insert(src_endpoint);
+        endpoint_key.insert(dst_endpoint);
+        endpoint_key.insert(src_endpoint);
 
         int size = get_size(&packet[16]);
 
-        packet_map[dst_endpoint].dst_count ++;
-        packet_map[dst_endpoint].dst_size += size;
+        endpoints[dst_endpoint].dst_count ++;
+        endpoints[dst_endpoint].dst_size += size;
 
-        packet_map[src_endpoint].src_count ++;
-        packet_map[src_endpoint].src_size += size;
+        endpoints[src_endpoint].src_count ++;
+        endpoints[src_endpoint].src_size += size;
+
+        auto back = make_pair(src_mac, dst_mac);
+        if (conversation_key.count(back) == 1){
+            conversations[back].src_count++;
+            conversations[back].src_size += size;
+        } else {
+            conversations[addr_to_addr].dst_count++;
+            conversations[addr_to_addr].dst_size += size;
+            conversation_key.insert(addr_to_addr);
+        }
     }
 
 	pcap_close(handle);
-    printf("Mac\t\t\t\t\tAddress\t\t\tPacket\t\tBytes\t\tTx Packets\tTx Bytes\tRx Packets\tRx Bytes\n");
 
-    for (auto k: key){
-    	for (auto i: k.first){
-    		printf("%02x ", i);
+    printf("Endpoints\n");
+    printf("Mac\t\t\tAddress\t\tPacket\t    Bytes\tTx Packets  Tx Bytes\tRx Packets  Rx Bytes\n");
+
+    for (auto endpoint: endpoint_key){
+    	for (auto mac: endpoint.first){
+    		printf("%02x ", mac);
     	} printf("\t");
     	
-    	for (auto i: k.second){
-    		printf("%02x ", i);
+    	for (auto ip_addr: endpoint.second){
+    		printf("%02x ", ip_addr);
     	} printf("\t");
 
-    	printf("%-12d%-12s", packet_map[k].dst_count + packet_map[k].src_count,
-    						get_calc_size(packet_map[k].dst_size + packet_map[k].src_size).c_str());
-    	printf("%-12d%-12s", packet_map[k].dst_count, get_calc_size(packet_map[k].dst_size).c_str());
-    	printf("%-12d%-12s", packet_map[k].src_count, get_calc_size(packet_map[k].src_size).c_str());
+        auto endp = endpoints[endpoint];
+
+    	printf("%-12d%-12s", endp.dst_count + endp.src_count, get_calc_size(endp.dst_size + endp.src_size).c_str());
+    	printf("%-12d%-12s", endp.dst_count, get_calc_size(endp.dst_size).c_str());
+    	printf("%-12d%-12s", endp.src_count, get_calc_size(endp.src_size).c_str());
     	printf("\n");
     }
 
+
+    printf("\n\nConversations\n");
+    printf("Address A\t\tAddress B\t\tPacket\t    Bytes\tTx Packets  Tx Bytes\tRx Packets  Rx Bytes\n");
+
+
+    for(auto conversation: conversation_key){
+        for (auto addr_a: conversation.first) {
+            printf("%02x ", addr_a);
+        } printf("\t");
+
+        for (auto addr_b: conversation.second) {
+            printf("%02x ", addr_b);
+        } printf("\t");
+        
+
+        auto conv = conversations[conversation];
+        printf("%-12d%-12s", conv.dst_count + conv.src_count, get_calc_size(conv.dst_size + conv.src_size).c_str());
+        printf("%-12d%-12s", conv.dst_count, get_calc_size(conv.dst_size).c_str());
+        printf("%-12d%-12s", conv.src_count, get_calc_size(conv.src_size).c_str());
+        printf("\n");    
+    }
 	return 0;
 }
